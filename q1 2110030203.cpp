88e9a894a2f86783d7fc6657dd82aa904247477c
Manisrireddy <109570089+Manisrireddy@users.noreply.github.com>
2022-07-27 11:20:49 +0530

    Add files via upload

---
+++ q1 2110030203.cpp
@@ -0,0 +1,11 @@
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+ #include<stdio.h>
+ int main()
+ {
+    int num;
+    scanf("%d",&num);
+    if(num%2==0)
+        printf("yes");
+    else printf("no");
+    return 0;
+}
